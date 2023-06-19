43455091c7449ef9cf703ea4a8872b918336a618
suditya <guptasuditya@gmail.com>
2023-06-19 14:59:44 +0530

    debuged some db changes

--- data/db.js
+++ data/db.js
@@ -6,9 +6,11 @@ const {NODE_ENV,DB_HOST,DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}$@{DB_HOST}/${DB_NAME}/?retryWrites=true&w=majority`
 
 
+
 // const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/?retryWrites=true&w=majority`
 
 // mongodb+srv://username:password@hostname/database
+
 
 mongoose.connect(connectionStr, {
     useNewUrlParser: true,
